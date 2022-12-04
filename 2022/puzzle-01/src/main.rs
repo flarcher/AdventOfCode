b51a95fc0758b17ec0c70578ceee1966318ef058
Fabrice Larcher <flarcher6@gmail.com>
2022-12-04 22:11:04 +0100

    Using functions in puzzle 01

--- 2022/puzzle-01/src/main.rs
+++ 2022/puzzle-01/src/main.rs
@@ -3,56 +3,60 @@ use std::io;
 
 static DEFAULT_FILTER : usize = 1;
 
+fn get_filter_count() -> usize {
+  let args: Vec<String> = env::args().collect();
+  if &args.len() > &1 {
+    let tmp = &args.get(1)
+      .map(|s| s.parse::<usize>())
+      .or(Option::Some(Result::Ok(DEFAULT_FILTER)))
+      .unwrap()
+      .expect("Internal error");
+    *tmp
+  }
+  else {
+    DEFAULT_FILTER
+  }
+}
+
+fn sort_aelves(stdin : io::Stdin, counters : &mut Vec<u32>) -> () {
+  let mut line = String::new();
+  let mut count : u32 = 0;
+  loop {
+      let bytes_count = stdin.read_line(&mut line).unwrap();
+      //println!("{}", line);
+      let value_str = line.trim_end();
+      if bytes_count <= 0 {
+          break;
+      }
+      else if value_str.is_empty() {
+          // New Aelve
+          counters.push(count);
+          count = 0;
+      }
+      else {
+          //println!("{}", line);
+          let line_value : u32 = value_str.parse().expect("Failed to parse line");
+          count += line_value
+      }
+      line.clear();
+  }
+
+  counters.sort_by(|l, r| l.cmp(r).reverse());
+}
+
+fn print_aelves(slice : &[u32], filter_count : usize) -> () {
+  let mut total : u32 = 0;
+  for count in &slice[0..filter_count] {
+      total += count;
+      println!("{}", count);
+  }
+  println!("Total is {}", total);
+}
+
 fn main() {
-
-    // Arguments
-    let args: Vec<String> = env::args().collect();
-    let filter_count : usize;
-    if &args.len() > &1 {
-      let tmp = &args.get(1)
-        .map(|s| s.parse::<usize>())
-        .or(Option::Some(Result::Ok(DEFAULT_FILTER)))
-        .unwrap()
-        .expect("Internal error");
-      filter_count = *tmp;
-    }
-    else {
-      filter_count = DEFAULT_FILTER;
-    }
-
-    // Parsing
-    let mut counters : Vec<u32> = Vec::new();
-    let mut line = String::new();
-    let stdin = io::stdin();
-    let mut count : u32 = 0;
-    loop {
-        let bytes_count = stdin.read_line(&mut line).unwrap();
-        //println!("{}", line);
-        let value_str = line.trim_end();
-        if bytes_count <= 0 {
-            break;
-        }
-        else if value_str.is_empty() {
-            // New Aelve
-            counters.push(count);
-            count = 0;
-        }
-        else {
-            //println!("{}", line);
-            let line_value : u32 = value_str.parse().expect("Failed to parse line");
-            count += line_value
-        }
-        line.clear();
-    }
-
-    // Output
-    counters.sort_by(|l, r| l.cmp(r).reverse());
-    let mut total : u32 = 0;
-    for count in &counters.as_slice()[0..filter_count] {
-        total += count;
-        println!("{}", count);
-    }
-
-    println!("Total is {}", total);
-
+    let stdin        : io::Stdin = io::stdin();
+    let filter_count : usize     = get_filter_count();
+    let mut counters : Vec<u32>  = Vec::new();
+    sort_aelves(stdin, &mut counters);
+    print_aelves(&counters.as_slice(), filter_count);
 }
